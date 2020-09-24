65872fa887e034159a1b78793e4066bc347a28b1
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-09-24 14:49:20 +0900

    Userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "半角英数のみが使えます"}, length: {minimum: 5}
-    validates :last_name
-    validates :first_name
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、カナ、漢字が使えます"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、カナ、漢字が使えます"}
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナのみが使えます"}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナのみが使えます"}
     validates :birthday
