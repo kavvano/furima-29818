ea69dbbff7826f60c27c43ceed8f4db0e3f94e21
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-09-24 14:09:51 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :links
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "半角英数のみが使えます"}, length: {minimum: 5}
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナのみが使えます"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナのみが使えます"}
+    validates :birthday
+  end
+  
 end
