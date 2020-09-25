0ca74d60ca6aa79469c7ef173751e7ad54aeb16d
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-09-25 11:06:57 +0900

    rubocop実行・コード整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,11 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password, format: { with: /\A(?=\d{0,254}+[a-z])(?=[a-z]{0,254}+\d)[a-z\d]{6,255}+\z/i, message: "半角英数のみが使えます"}, length: {minimum: 5}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、カナ、漢字が使えます"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、カナ、漢字が使えます"}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナのみが使えます"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナのみが使えます"}
+    validates :password, format: { with: /\A(?=\d{0,254}+[a-z])(?=[a-z]{0,254}+\d)[a-z\d]{6,255}+\z/i, message: '半角英数のみが使えます' }, length: { minimum: 5 }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな、カナ、漢字が使えます' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな、カナ、漢字が使えます' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナのみが使えます' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナのみが使えます' }
     validates :birthday
   end
-  
 end
