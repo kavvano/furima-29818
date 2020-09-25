815a85fac3c99aa1a61fc9cf6f3fea53bf418204
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-09-25 10:58:41 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Internet.username}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"123abc"}
     password_confirmation {password}
     first_name            {Gimei.name.first.kanji}
     last_name             {Gimei.name.last.kanji}
