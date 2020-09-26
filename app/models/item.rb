3809b6dc6cf827879594778ec8c0aaa8e0d034e2
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-09-26 23:04:34 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title, length: { maximum: 40 }
     validates :explanation, length: { maximum: 1000 }
-    validates :category, :status, :delivery_fee, :area, :days, :user
-    validates :price, length: { in: 300..9999999}
+    validates :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, numericality: { other_than: 1 }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :image, :user
   end
-  validates :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, numericality: { other_than: 1 }
 
 end
