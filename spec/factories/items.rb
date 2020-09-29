d5004be2bf86e5beac8731f923a81eb8c57ba9cf
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-09-29 16:16:59 +0900

    itemsテーブルのdays_idカラム名day_idへ変更

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     status_id       { Faker::Number.within(range: 2..7) }
     delivery_fee_id { Faker::Number.within(range: 2..3) }
     area_id         { Faker::Number.within(range: 2..48) }
-    days_id         { Faker::Number.within(range: 2..4) }
+    day_id          { Faker::Number.within(range: 2..4) }
     price           { Faker::Number.within(range: 300..9_999_999) }
     association     :user
   end
