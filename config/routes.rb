45be8461f91dab127a6c0f1b866481e9450718ab
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-01 18:26:45 +0900

    Revert "linksコントローラーのルーティング設定"
    This reverts commit fab97305efe2bff56df9fa3c7806ecb41d53bbbe.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   get 'links/index'
   devise_for :users
   root to: "items#index"
-  resources :items do
-    resources :links, only: :index
-  end
+  resources :items
 end
