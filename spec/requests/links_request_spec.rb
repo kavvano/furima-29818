2c3ccf7affde06b8ca30b90c65033f55e6e775f0
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-01 18:07:20 +0900

    linksコントローラー作成

---
+++ spec/requests/links_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Links", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/links/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
