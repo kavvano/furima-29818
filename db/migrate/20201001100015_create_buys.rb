620c935e5530564cdb07174aab7b12b02f6b2bd2
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-02 14:24:52 +0900

    buysテーブルのカラム名修正

--- db/migrate/20201001100015_create_buys.rb
+++ db/migrate/20201001100015_create_buys.rb
@@ -1,7 +1,13 @@
 class CreateBuys < ActiveRecord::Migration[6.0]
   def change
     create_table :buys do |t|
-
+      t.string :post_num, null: false
+      t.integer :area_id, null: false
+      t.string :city,     null: false
+      t.string :address,  null: false
+      t.string :building
+      t.string :cell_num, null: false
+      t.references :link, null: false, foreign_key: true
       t.timestamps
     end
   end
