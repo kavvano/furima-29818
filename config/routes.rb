f9c18aa1dc31b86d79fbe08f792a918dd69aae17
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-03 15:15:56 +0900

    linksのcreateアクションへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :links, only: :index
+    resources :links, only: [:index, :create]
   end
 end
