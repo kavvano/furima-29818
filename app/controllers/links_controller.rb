c8064323e746fbc25c25c6f0ed979bbe26c56e72
kavvano <kyawano@seagreen.ocn.ne.jp>
2020-10-04 12:57:47 +0900

    購入画面表示・購入機能実装

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,39 @@
 class LinksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
   def index
+    @link_item = LinkItem.new
+  end
+
+  def create
+    @link_item = LinkItem.new(link_params)
+    if @link_item.valid?
+      @link_item.save
+      pay_item
+      @item.update(sold_out: true)
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link_item).permit(:post_num, :area_id, :city, :address, :building, :cell_num).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: link_params[:token],
+      currency: 'jpy'
+    )
   end
 end
